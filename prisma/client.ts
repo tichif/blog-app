ce4e0a2b9f81289c252674400acb3cc655b318af
Dalzon Charles-Hébert <charlby5@gmail.com>
2023-02-21 08:28:20 -0500

    Define a prisma client

---
+++ prisma/client.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from '@prisma/client';
+
+// @ts-ignore
+const client = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+  // @ts-ignore
+  globalThis.prisma = client;
+}
+
+export default client;
