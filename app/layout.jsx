c2e88ff117c88557fe437a19305c9945e57c00f4
Dalzon Charles-Hébert <charlby5@gmail.com>
2023-02-21 20:17:22 -0500

    Begin with the nav and login functionnality

---
+++ app/layout.jsx
@@ -0,0 +1,28 @@
+import { Roboto } from '@next/font/google';
+
+import './globals.css';
+import Nav from './auth/Nav';
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-roboto',
+});
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang='en'>
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body
+        className={`mx-4 md:mx-48 xl: mx-96 ${roboto.variable} bg-gray-200`}
+      >
+        <Nav />
+        {children}
+      </body>
+    </html>
+  );
+}
