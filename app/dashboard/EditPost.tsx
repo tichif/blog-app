258f7e76857294cfebb300f530025e108346576a
Dalzon Charles-Hébert <charlby5@gmail.com>
2023-02-22 19:58:32 -0500

    Add delete button

--- app/dashboard/EditPost.tsx
+++ app/dashboard/EditPost.tsx
@@ -31,6 +31,15 @@ const EditPost = ({ avatar, name, postTitle, id, comments }: Props) => {
         />
         <h3 className='font-bold text-gray-700'>{name}</h3>
       </div>
+      <div className='my-8'>
+        <p className='break-all'>{postTitle}</p>
+      </div>
+      <div className='flex items-center gap-4'>
+        <p className='text-sm font-bold text-gray-700'>
+          {comments?.length} Comments
+        </p>
+        <button className='text-sm font-bold text-red-500'>Delete</button>
+      </div>
     </div>
   );
 };
